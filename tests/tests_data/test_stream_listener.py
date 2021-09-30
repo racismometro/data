90a6a18c9877892d08920366dd22570c7bf16256
Gustavo Jardim <gustavo.jardim@thoughtworks.com>
2021-09-30 16:28:44 -0300

    Removing comments from test_stream_listener

--- tests/tests_data/test_stream_listener.py
+++ tests/tests_data/test_stream_listener.py
@@ -3,16 +3,16 @@ from mock import patch
 
 import json
 
-# @patch('data.data_lake.DataLake')
-# def test_should_return_exact_data_from_stream(data_lake_mock):
-#     raw_data = {
-#         'text': 'this is a mock tweet',
-#         'in_reply_to_status_id': 1223
-#     }
-#     raw_data_json = json.dumps(raw_data)
+@patch('data.data_lake.DataLake')
+def test_should_return_exact_data_from_stream(data_lake_mock):
+    raw_data = {
+        'text': 'this is a mock tweet',
+        'in_reply_to_status_id': 1223
+    }
+    raw_data_json = json.dumps(raw_data)
     
-#     tweet_streamer = TweetStreamer(data_lake_mock)
+    tweet_streamer = TweetStreamer(data_lake_mock)
 
-#     tweet_streamer.on_data(raw_data_json)
+    tweet_streamer.on_data(raw_data_json)
 
-#     data_lake_mock.insert.assert_called_with(raw_data)
+    data_lake_mock.insert.assert_called_with(raw_data)
