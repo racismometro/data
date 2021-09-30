163b319e6eabb216820428a77bf0d4535c6f668f
Gustavo Jardim <gustavo.jardim@thoughtworks.com>
2021-09-30 16:28:26 -0300

    Update twitter_api.py

--- src/data/core/twitter_api.py
+++ src/data/core/twitter_api.py
@@ -1,16 +1,18 @@
 import os
-import tweepy
+
+from tweepy.api import API
 
 class TwitterApi():
 
-    @staticmethod
-    def get_api() -> tweepy.API:
+    def get_api_credentials(self):
         API_KEY = os.getenv('API_KEY')
         API_SECRET = os.getenv('API_SECRET')
         ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
         ACCESS_SECRET = os.getenv('ACCESS_SECRET')
 
-        auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-        auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-        
-        return tweepy.API(auth)
+        return {
+            'consumer_key' : API_KEY,
+            'consumer_secret' : API_SECRET,
+            'access_token' : ACCESS_TOKEN,
+            'acess_secret' : ACCESS_SECRET
+        }
