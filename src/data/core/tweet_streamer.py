95226c491a789cdfb8835d29c035c6888879903e
Gustavo Jardim <gustavo.jardim@thoughtworks.com>
2021-09-30 16:28:10 -0300

    Update tweet_streamer.py

--- src/data/core/tweet_streamer.py
+++ src/data/core/tweet_streamer.py
@@ -1,13 +1,18 @@
+import os
 import tweepy
 
+from data.core.twitter_api import TwitterApi
 from data.data_lake import DataLake
 
 
 class TweetStreamer(tweepy.Stream):
-    ...
-    # def __init__(self, data_lake: DataLake, api=None):
-    #     super().__init__(api)
-    #     self.__data_lake = data_lake
+    def __init__(self, data_lake: DataLake, api=None):
+        twitter_api = TwitterApi()
 
-    # def on_status(self, status):
-    #     self.__data_lake.insert(status._json)
+        consumer_key, consumer_secret, access_token, access_secret = twitter_api.get_api_credentials().values()
+
+        super().__init__(consumer_key, consumer_secret, access_token, access_secret)
+        self.__data_lake = data_lake
+
+    def on_status(self, status):
+        self.__data_lake.insert(status._json)
