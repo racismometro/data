7794b8f28865ae6a41356334727bcb12f7724715
Gustavo Jardim <gustavo.jardim@thoughtworks.com>
2021-10-01 11:18:59 -0300

    Update test_tweets_endpoints.py

--- tests/tests_api/test_tweets_endpoints.py
+++ tests/tests_api/test_tweets_endpoints.py
@@ -23,8 +23,8 @@ def mocked_service():
 @patch('configparser.RawConfigParser.getboolean')
 def test_total_incidence_between_dates_toggle_on(mocked_getboolean, mocked_service):
     mocked_getboolean.return_value = True
-    
-    response = client.get('/total_incidence_between_dates/start-date=06-01-2021&end-date=06-31-2021')
+
+    response = client.get('refined_data/total_incidence_between_dates/start-date=06-01-2021&end-date=06-31-2021')
 
     assert response.status_code == 200
     assert response.json() == { 'body' : 3765 }
@@ -33,7 +33,7 @@ def test_total_incidence_between_dates_toggle_on(mocked_getboolean, mocked_servi
 def test_total_incidence_between_dates_toggle_off(mocked_getboolean, mocked_service):
     mocked_getboolean.return_value = False
 
-    response = client.get('/total_incidence_between_dates/start-date=06-01-2021&end-date=06-31-2021')
+    response = client.get('refined_data/total_incidence_between_dates/start-date=06-01-2021&end-date=06-31-2021')
 
     assert response.status_code == 200
     assert response.json() == { 'body' : 'feature is disabled' }
