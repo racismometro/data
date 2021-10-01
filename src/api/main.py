0588e2500e179a81ca69f43d2167b80bb7c21b04
Gustavo Jardim <gustavo.jardim@thoughtworks.com>
2021-10-01 11:18:55 -0300

    removed some routes from main and added to a separated file

--- src/api/main.py
+++ src/api/main.py
@@ -1,27 +1,9 @@
-from fastapi import Request, FastAPI
-from api.service.tweet_service import TweetService
-
-import configparser
+from fastapi import FastAPI
+from api.controller import refined_data_controller
 
 app = FastAPI()
-
-tweet_service = TweetService()
-
-config = configparser.RawConfigParser()
-config.read('toggles.properties')
-incidence_counter_toggle = config.getboolean("toggles", "incidence_counter")
+app.include_router(refined_data_controller.router, prefix="/refined_data")
 
 @app.get("/")
 async def root():
     return {"body": "I am free!!!"}
-
-@app.get("/total_incidence_between_dates/start-date={start_date}&end-date={end_date}")
-async def get_total_incidence_between_dates(start_date, end_date):
-    if config.getboolean("toggles", "incidence_counter"):
-        total_count = tweet_service.get_total_incidence_between_dates()
-        return {
-            "body" : total_count
-        }
-    return {
-            "body" : "feature is disabled"
-    }
