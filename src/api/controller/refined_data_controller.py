954cf141f8bb5b265dd26b3a469d875b422769dd
Gustavo Jardim <gustavo.jardim@thoughtworks.com>
2021-10-01 11:18:34 -0300

    Create refined_data_controller.py

---
+++ src/api/controller/refined_data_controller.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+from api.service.tweet_service import TweetService
+
+import configparser
+
+tweet_service = TweetService()
+
+config = configparser.RawConfigParser()
+config.read('toggles.properties')
+
+router = APIRouter()
+
+@router.get("/total_incidence_between_dates/start-date={start_date}&end-date={end_date}")
+async def get_total_incidence_between_dates(start_date, end_date):
+    if config.getboolean("toggles", "incidence_counter"):
+        total_count = tweet_service.get_total_incidence_between_dates()
+        return {
+            "body" : total_count
+        }
+    return {
+            "body" : "feature is disabled"
+    }
