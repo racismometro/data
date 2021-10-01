e21aa0690be4a0b5eb0153932ff6546f042c4155
Gustavo Jardim <gustavo.jardim@thoughtworks.com>
2021-10-01 10:46:23 -0300

    Adicionando suporte a feature toggles nos testes

--- tests/tests_api/test_tweets_endpoints.py
+++ tests/tests_api/test_tweets_endpoints.py
@@ -1,8 +1,11 @@
+
 from mock import patch
 from api.main import app
 from fastapi.testclient import TestClient
-import pytest
 from api.service.tweet_service import TweetService
+
+import pytest
+
 client = TestClient(app)
 
 def test_api_is_up():
@@ -17,9 +20,20 @@ def mocked_service():
         service = TweetService()
         yield service
         
-def test_total_incidence_between_dates(mocked_service):
-    response = client.get('/total_incidence_between_dates')
+@patch('configparser.RawConfigParser.getboolean')
+def test_total_incidence_between_dates_toggle_on(mocked_getboolean, mocked_service):
+    mocked_getboolean.return_value = True
+    
+    response = client.get('/total_incidence_between_dates/start-date=06-01-2021&end-date=06-31-2021')
 
     assert response.status_code == 200
     assert response.json() == { 'body' : 3765 }
 
+@patch('configparser.RawConfigParser.getboolean')
+def test_total_incidence_between_dates_toggle_off(mocked_getboolean, mocked_service):
+    mocked_getboolean.return_value = False
+
+    response = client.get('/total_incidence_between_dates/start-date=06-01-2021&end-date=06-31-2021')
+
+    assert response.status_code == 200
+    assert response.json() == { 'body' : 'feature is disabled' }
